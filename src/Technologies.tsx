2700b387f11215029a742127c82388f6aa961ed5
Sulakshin2001 <nikitasulaaskin@gmail.com>
2022-07-18 11:42:40 +0300

    Initial commit first

---
+++ src/Technologies.tsx
@@ -0,0 +1,12 @@
+export const Technologies = ()=>{
+    return(
+        <div>
+            <ul>
+                <li>dsd</li>
+                <li>dsd</li>
+                <li>dsd</li>
+                <li>dsd</li>
+            </ul>
+        </div>
+    )
+}
